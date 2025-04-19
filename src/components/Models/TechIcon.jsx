b4949e4e39e3fcf0b47090b5f7040332d623fc66
Étienne CHEVALIER <etiennechevalier@Mac.home>
2025-04-18 22:35:18 -0500

    first commit

---
+++ src/components/Models/TechIcon.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect } from 'react'
+import { Environment, Float, OrbitControls, useGLTF } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber';
+import * as THREE from 'three';
+
+// - Loads a `.glb` model (GLTF format)
+// - Adds basic lighting
+// - Applies environment reflections for realism
+// - Wraps the model in a floating animation
+// - Optionally tweaks the material (lie setting a white color on a specific mesh)
+// - Disables zoom using OrbitControls
+
+const TechIcon = ({ model }) => {
+    const scene = useGLTF(model.modelPath);
+
+    useEffect(() => {
+        if(model.name === 'Interactive Developer') {
+            scene.scene.traverse((child) => {
+                if(child.isMesh && child.name === 'Object_5') {
+                    child.material = new THREE.MeshStandardMaterial({color: 'white'})
+                }
+            })
+        }
+    })
+
+    return (
+        <Canvas>
+            <ambientLight intensity={0.3}/>
+            <directionalLight position={[5, 5, 5]} intensity={1} />
+
+            <Environment preset="city" />
+
+            <OrbitControls enableZoom={false} />
+
+            <Float speed={5.5} rotationIntensity={0.5} floatIntensity={0.9}>
+                <group scale={model.scale} rotation={model.rotation}>
+                    <primitive object={scene.scene} />
+                </group>
+            </Float>
+        </Canvas>
+    )
+}
+
+export default TechIcon
